61dd889c77716e4d02639d1a5f6aa0ae012ad689
Elmo <hed-fevaleros@smu.edu.ph>
2025-03-05 12:32:55 +0800

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonolithApp.Demo.Models;
+using MonolithApp.Demo.Services;
+
+namespace MonolithApp.Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ProductsService _productsService;
+        public ProductsController()
+        {
+            _productsService = new ProductsService();
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ProductId = TempData["ProductId"]?.ToString();
+            return View();
+        }
+
+        public IActionResult Details([FromQuery] int id)
+        {
+            try
+            {
+                Product product = _productsService.GetProductById(id);
+                return View(product);
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SubmitProduct(Product product)
+        {
+            _productsService.AddProduct(product);
+            TempData["ProductId"] = product.Id;
+            return RedirectToAction("Index");
+        }
+
+		public IActionResult Edit([FromQuery] int id)
+		{
+			var product = _productsService.GetProductById(id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return View(product);
+		}
+
+
+		[HttpPost]
+		public IActionResult Edit(int id, Product product)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(product);
+			}
+
+			_productsService.UpdateProduct(product);
+            return Redirect($"/Products/Details?id={product.Id}");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = _productsService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productsService.DeleteProduct(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Services/ProductsService.cs
@@ -0,0 +1,41 @@
+﻿using MonolithApp.Demo.Models;
+using SQLite;
+
+namespace MonolithApp.Demo.Services
+{
+    public class ProductsService
+    {
+        private readonly SQLiteConnection db =
+            new SQLiteConnection("Products.db");
+
+        public ProductsService()
+        {
+            db.CreateTable<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            db.Insert(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            db.Update(product);
+        }
+
+        public void DeleteProduct(int Id)
+        {
+            var product = GetProductById(Id);
+            if (product != null)
+            {
+                db.Delete(product);
+            }
+        }
+
+        public Product GetProductById(int id){
+            TableQuery<Product> result = 
+                db.Table<Product>().Where(q => q.Id.Equals(id));
+            return result.FirstOrDefault();
+        }
+    }
+}
